f409304554d8eda5c08725c192be6570fddbfb4a
TheKenster1729 <48304658+TheKenster1729@users.noreply.github.com>
2021-02-19 11:10:23 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import numpy as np
 
 st.title('Q-Week Results')
 
-df = pd.read_html('https://docs.google.com/spreadsheets/d/1UCrSZrIX45MlKz6EQfEVJ-9dHjLw7xeTpg77lujknEw/edit#gid=1779447167', flavor='bs4', encoding='utf8', skiprows=[0])[0]
+df = pd.read_csv('https://docs.google.com/spreadsheets/d/1UCrSZrIX45MlKz6EQfEVJ-9dHjLw7xeTpg77lujknEw/export?format=csv&gid=1779447167')
 
 df = df[[col for col in df.columns if 'Unnamed' not in col and col != '1']]
 df = df.dropna(axis = 'index', how = 'all')
@@ -33,32 +33,18 @@ if 'All' not in events:
 
 df['Kerb'] = 'bottom text'
 
-# for each quad, get the events they've been to and the participation points
-
-# for each quad, sum points over all events
-
 quad_groups = df.groupby('Quad')
 for quad, group in sorted(quad_groups, key=lambda x: str(x[1]['Quad'])):
     
-    points_df = group.groupby('Event').agg('max')
+    points_df = group.groupby('Event').agg('min')
     event_names = points_df.index.values
     puntos = [int(i)/int(j)*400 for i, j in zip(points_df['Participation'].to_list(), points_df['Quad Total'].to_list())]
+    if '2/16: Opening Ceremony' in event_names:
+        puntos[list(event_names).index('2/16: Opening Ceremony')] *= 5/4 
     display_df = {'Event Name': event_names, 'Attendance Points': puntos}
 
     f'### {quad}'
-    f"**Total Points**: {sum(puntos)}"
-
-    # display_df['Event Points'] = int(group.groupby('Event').agg('min')['Participation'])/int(group.groupby('Event').agg('min'))*400
-
-
-    # for event_frame in group.groupby('Event').agg({'min'}):
-    #     print(event_frame)
-    #     if event_frame.shape[0] == 1:
-    #         display_df['Event Name'].append(event_name)
-    #         display_df['Attendance Points'].append(int(event_frame['Participation'])/int(event_frame['Quad Total'])*400)
-    #     else:
-    #         display_df['Event Name'].append(event_name)
-    #         points = event_frame.groupby('Event').agg({'Participation': 'max'}).iloc[0]
+    f"**Total Points**: {format(sum(puntos),'.2f')}"
 
     display_df = pd.DataFrame(display_df)
     display_df = display_df.sort_values(by=['Attendance Points'], ascending=False).reset_index(drop=True)
